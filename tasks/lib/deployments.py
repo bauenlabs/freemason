11853b175f49bcc6f18a8a0678615adce043615e
Grant Campbell <grant@pulsr.com>
2017-04-15 15:01:36 -0500

    deployment task can now clone a repository

--- tasks/lib/deployments.py
+++ tasks/lib/deployments.py
@@ -10,7 +10,6 @@ class Deployment(object):
     """
     The Initialization Function
     """
-    print
     self.repository = kwargs['repository']
     self.path = kwargs['path']
     self.branch = kwargs['branch']
@@ -23,18 +22,74 @@ class Deployment(object):
     This function clones a repository, pulls the needed branch then sets that as 
     the HEAD in the repo
     """
-    # Use the Git binary to clone the repo at the set temp path
-    Git().clone(self.repository, self.path)
-  
-    # Instantiate a Repo objet for the newly cloned repo
-    r = Repo(self.path)
-    
-    # Ensure that the cloned repo is up-to-date
-    r.remotes.origin.pull()
-  
-    # Checkout the correct branch
-    r.create_head(self.branch, 'HEAD')
-    r.heads[0].checkout()
+    try:
+      # Use the Git binary to clone the repo at the set temp path
+      Git().clone(self.repository.encode('utf-8'), self.path.encode('utf-8'))
+      # Emit sucessful clone event
+      self.socketio.emit('deployment_event', {"data": "Repo Cloned", 
+        "room": self.task_id, "progress_percent": "25%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+    except Exception as e:
+      # Emit failure event
+      self.socketio.emit('deployment_event', {"data": "failed to clone repo: {0}".format(e), 
+        "room": self.task_id, "progress_percent": "0%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+      # Return Failure message and exception
+      return "failed to clone repo"
+    try:
+      # Instantiate a Repo object for the newly cloned repo
+      r = Repo(self.path.encode('utf-8'))
+      # Emit sucessful instantiation event
+      self.socketio.emit('deployment_event', {"data": "Repo Instantiation complete", 
+        "room": self.task_id, "progress_percent": "50%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+    except Exception as e:
+      # Emit failure event
+      self.socketio.emit('deployment_event', {"data": "failed to instatiate repo: {0}".format(e), 
+        "room": self.task_id, "progress_percent": "0%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+      # Return Failure message and exception
+      return "failed to instantiate repo"
+
+    try: 
+      # Ensure that the cloned repo is up-to-date
+      r.remotes.origin.pull()
+      # Emit sucessful pull  event
+      self.socketio.emit('deployment_event', {"data": "Repo is up to date with origin", 
+        "room": self.task_id, "progress_percent": "75%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+    except Exception as e:
+      # Emit failure event
+      self.socketio.emit('deployment_event', {"data": "failed to pull remote origin: {0}".format(e), 
+        "room": self.task_id, "progress_percent": "0%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+      # Return Failure message and exception
+      return "failed to bring repo up to date"
+    try:
+      # Checkout the correct branch
+      r.create_head(self.branch.encode('utf-8'), 'HEAD')
+      r.heads[0].checkout()
+      # Emit sucessful pull  event
+      self.socketio.emit('deployment_event', {"data": "Sucessfully checkedout {0}".format(self.branch),
+        "room": self.task_id, "progress_percent": "100%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+    except Exception as e:
+      # Emit failure event
+      self.socketio.emit('deployment_event', {"data": "failed to checkout {1}: {0}".format(e,self.branch), 
+        "room": self.task_id, "progress_percent": "0%" }, 
+        room=self.task_id, namespace='/deployments'
+      )
+      # Return Failure message and exception
+      return "failed to checkout {0}".format(self.branch)
+
+
   def buildImage(self):
     """
     This function builds a docker image based on the Dockerfile in the directory 
@@ -50,12 +105,5 @@ class Deployment(object):
     This function is what is envoked by celery itself when a deployment task is
     executed.
     """
-    import time
-    for x in xrange(10):
-      time.sleep(2)
-      currentID = self.task_id
-      socketio = SocketIO(message_queue="redis://")
-      percent = (x + 1) * 10
-      string = "{0}%".format(percent)
-      socketio.emit('deployment_event', {"data": currentID, "room": currentID, "percent": string }, room=currentID, namespace='/deployments') 
-    return "progessiveTaskExample is done"
+    test = self.cloneRepo() 
+    return test
